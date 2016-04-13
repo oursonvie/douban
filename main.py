290379588c23620f2d17c7fac627f4a414838267
oursonvie <oursonvie@gmail.com>
2016-04-13 01:13:46 +0000

    starting

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import crawler
+
+# homepage of a user on douban
+homepage = 'https://www.douban.com/people/sucks1990/'
+
+def getMusic():
+
+
+
+def main():
+    getMusic()
+
+if __name__ == "__main__":
+    main()
